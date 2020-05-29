950f375fabde6d216b891b01b12028ff1778dee5
Jeff <jeffrey.austria1121@gmail.com>
2020-05-30 04:44:59 +0800

    first commit

---
+++ app_class.js
@@ -0,0 +1,70 @@
+
+
+const cityForm = document.querySelector('form');
+
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const time = document.querySelector('.time');
+const icon = document.querySelector('.icon img');
+const forecast = new Forecast();
+
+
+console.log(forecast);
+const updateUI = (data) => {
+    // const cityDets = data.cityDets;
+    // const weather = data.weather; or destructuring
+
+    const { cityDets, weather} = data;
+ 
+
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>
+    `;
+
+    //update day or night images && icon images
+    // let timeSrc = null;
+    // if(weather.IsDayTime){
+    //     timeSrc = 'img/day.png';
+    // }
+    // else{
+    //     timeSrc = 'img/night.svg';
+    // } or ternary operator
+    let timeSrc = weather.IsDayTime ? 'img/day.png' :  'img/night.svg';
+    time.setAttribute("src", timeSrc);
+
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute("src", iconSrc);
+
+    // if contains d-none class remove it
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+}
+
+
+cityForm.addEventListener("submit", e =>{
+    e.preventDefault();
+
+    //get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+
+    //update the ui with new city
+    forecast.updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+
+    localStorage.setItem("location", city);
+});
+
+if(localStorage.getItem('location')){
+    forecast.updateCity(localStorage.getItem('location'))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+}
---
+++ scripts/forecast.js
@@ -0,0 +1,84 @@
+const key = 'bt6a9AIn2vJk0abyjshxBLHWTKXYHuFe';
+
+// ; 
+// // get location information
+// const getWeather = async (id) =>{
+//     const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+//     const query =  `${id}?apikey=${key}`;
+
+//     const response = await fetch(base + query);
+//     const data = await response.json();
+
+//     return data[0];
+// }
+
+
+
+// //get city information
+// const getCity = async (city) =>{
+//     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+//     const query = `?apikey=${key}&q=${city}`;
+
+//     const response = await fetch(base + query);
+//     const data = await response.json();
+
+//     return data[0];
+// }
+
+// getCity('manchester')
+// .then( data => {
+//     return getWeather(data.Key);
+// }).then( data => {
+//     console.log(data);
+// })
+// .catch( err => console.log(err));
+
+// const getWeather = async (id) => {
+//     base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+//     query = `${id}?apikey=${key}`;
+
+//     const response = await fetch(base + query);
+//     const data = await response.json();
+
+//     return data[0];
+// }
+// const getCity = async (city)=>{
+//     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+//     const query = `?apikey=${key}&q=${city}`;
+
+//     const response = await fetch(base + query);
+//     const data = await response.json();
+
+//     return data[0];
+// }
+
+// getCity('manchester')
+// .then( data => {
+//     return getWeather(data.Key);
+// }).then ( data => {
+//     console.log(data);
+// });
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query =  `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+}
+
+// getCity('manchester')
+// .then( data => {
+//     return getWeather(data.Key);
+// }).then( data => console.log(data))
+// .catch( err => console.log(err));
